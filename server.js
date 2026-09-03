806a1e93c409ad841b980b5b7e7ff361c6fd4a32
Abdul Basith <159468770+basthep@users.noreply.github.com>
2026-09-03 21:10:38 +0530

    Update CORS middleware configuration

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/images", express.static("public/images"));
 
